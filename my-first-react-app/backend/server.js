c337cb2d33274cc91415c2a96856994831d15840
Corey He <corey72he@gmail.com>
2025-02-10 12:02:55 -0500

    Implemented tracked searches for trends

--- my-first-react-app/backend/server.js
+++ my-first-react-app/backend/server.js
@@ -1,13 +1,102 @@
-const express=require("express");
+const express = require("express");
 const mongoose = require('mongoose');
-const cors=require('cors')
-const dotenv=require('dotenv');
-const todoModel=require('./models/todoModel')
+const cors = require('cors')
+const dotenv = require('dotenv');
+const searchModel = require('./models/searchTerm')
 
 dotenv.config();
 
-const app=express();
+const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cors());
+app.options('*', cors());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    })
+
+
+app.put('/api/updateMovie', cors(), async (req, res) => {
+    //console.log("Who is this")
+    //console.log(req);
+    try {
+        const query = req.body.data.query;
+        //console.log(query);
+        const movieData = req.body.data.queryResult
+        //console.log(movieData)
+
+        updateSearchCount(query, movieData);
+        res.status(200).json({ message: "Success", status: 200 });
+    }
+    catch (error) {
+        console.log(error)
+        res.status(400).json({ error: 'Failed to update count' });
+    }
+})
+
+
+app.get("/api/getTrendingMovies", async(req, res) => {
+    console.log("I was called")
+    try {
+        const movies = await getTrendingMovies();
+        //console.log(movies);
+        res.status(200).json({movies})
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to retrieve ToDos' });
+    }
+})
+
+const updateSearchCount = async (searchTerm, movie) => {
+    try {
+        const result = await searchModel.find({ queryText: `${searchTerm}` });
+        //console.log(result);
+        if (result.length > 0) {
+            const doc = result[0];
+            const oldCount = doc.count
+            //console.log(oldCount);
+            //console.log(doc.id);
+            const newResult = await searchModel.findByIdAndUpdate(doc.id, { count: oldCount + 1 });
+            //console.log(newResult)
+            console.log("Successfully updated query count");
+        }
+        else {
+            const newResult = new searchModel({
+                queryText: searchTerm,
+                count: 1,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+                movie_id: movie.id
+            })
+            newResult.save();
+            console.log('Saved query to MongoDB');
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
+const getTrendingMovies = async () => {
+    try {
+        const result = await searchModel.find({}).sort({count:-1}).limit(5)
+        return result;
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+
+app.listen(port, () => {
+    console.log("Listening");
+})
 
 //Steps to starting a new node.js
 //Initialize new folder
--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,102 +1,159 @@
 import Search from './components/Search.jsx'
-import React, {useEffect, useState} from 'react'
-import Spinner from'./components/Spinner.jsx'
+import React, { useEffect, useState } from 'react'
+import Spinner from './components/Spinner.jsx'
 import MovieCard from './components/MovieCard.jsx';
-import {useDebounce} from 'react-use';
+import { useDebounce } from 'react-use';
+import axios from "axios";
 
-const API_BASE_URL='https://api.themoviedb.org/3/';
+const API_URL = "http://localhost:8080/api";
+const api = axios.create({ baseURL: API_URL });
+
+const API_BASE_URL = 'https://api.themoviedb.org/3/';
 // discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const API_OPTIONS={
+const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
     Authorization: `Bearer ${API_KEY}`
   }
 }
-const App= () => {
-  const[searchTerm,setSearchTerm]=useState('')
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
 
-  const[errorMessage,setErrorMessage]=useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const [movieList, setMovieList]=useState([]);
-  const [isLoading, setIsLoading]=useState(false);
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [debouncedSearchTerm, setDebouncedSearchTerm]=useState('')
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
 
   //Debounce the earch term to prevent making too many API requests by waiting 500ms after the user stops typing.
-  useDebounce(()=> setDebouncedSearchTerm(searchTerm),500,[searchTerm])
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
 
+  const [trendingMovies, setTrendingMovies] = useState([]);
 
-  const fetchMovies=async(query='')=>{
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
 
-    try{
-      const endpoint=query 
-      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1`
-      :`${API_BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+    try {
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1`
+        : `${API_BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
 
-      const response = await fetch (endpoint, API_OPTIONS)
+      const response = await fetch(endpoint, API_OPTIONS)
 
-      if(!response.ok){
+      if (!response.ok) {
         throw new Error('Failed to fetch movies');
       }
-      const data=await response.json();
+      const data = await response.json();
 
-      if (data.Response==='False'){
+      if (data.Response === 'False') {
         setErrorMessage(data.Error || 'Failed to fetch movies');
-        setMovieList ([]);
+        setMovieList([]);
         return;
       }
       setMovieList(data.results || []);
-      console.error(data)
+
+      if (query && data.results.length > 0) {
+        console.log(query)
+        //console.log(data.results[0])
+        axios.put(`${API_URL}/updateMovie`, {
+          headers: { 'Content-Type': 'application/json' },
+          data: {
+            query: `${query}`,
+            queryResult: data.results[0]
+          }
+        })
+      }
     }
-    catch(error){
+    catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
     }
-    finally{
+    finally {
       setIsLoading(false);
     }
   }
 
-  useEffect(()=>{
-      fetchMovies(debouncedSearchTerm);
-  },[debouncedSearchTerm])
 
-    return (
-        <main>
-          <div className="pattern"/>
 
-          <div className="wrapper">
-            <header>
-              <img src='./hero.png' alt="Hero Banner"/>
-              <h1>Find <span className="text-gradient">Movies</span> You'll enjoy Without the Hassle</h1>
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm])
 
-              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
 
-            </header>
-          <section className="all-movies">
-            <h2 className="mt-[40px]">All Movies</h2>
-            {/* Conditional statement, if error do X. */}
+  const loadTrendingMovies = async () => {
+    setIsLoading(true);
+    //console.log("I am making a call to:"+`${API_URL}/getTrendingMovies`)
+    try {
+      const result = await axios.get(`${API_URL}/getTrendingMovies`);
+      console.log(result)
+      const movies = result.data.movies
+      setTrendingMovies(movies)
+    }
+    catch (error) {
+      console.log(error)
+    }
+    finally {
+      setIsLoading(false);
+    }
+  }
 
-            {isLoading ?(
-              <Spinner/>
-            ): errorMessage?(
-              <p className="text-red-500">{errorMessage}</p>
-            ):(
-              <ul>
-                {movieList.map((movie)=>(
-                  <MovieCard key ={movie.id} movie={movie}/>
-                ))}
-              </ul>
-            )}
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
+
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src='./hero.png' alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll enjoy Without the Hassle</h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+        </header>
+
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
           </section>
+        )}
 
-        </div>
-        </main>
-        );
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All Movies</h2>
+          {/* Conditional statement, if error do X. */}
+
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
+
+      </div>
+    </main>
+  );
 }
- 
+
 export default App;
